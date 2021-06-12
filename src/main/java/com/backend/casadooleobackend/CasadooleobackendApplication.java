c73336fe33122e1714c509cd6e5914f32fb1f402
Bruno <brunodornelesdeabreu@hotmail.com>
2021-06-12 19:44:13 -0300

    primeiro

---
+++ src/main/java/com/backend/casadooleobackend/CasadooleobackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.casadooleobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadooleobackendApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(CasadooleobackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/casadooleobackend/CasadooleobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.casadooleobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadooleobackendApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
